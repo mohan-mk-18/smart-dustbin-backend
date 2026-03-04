deb09740eb3d6b33c7f87d36240d4739ed23de2a
mohan-mk-18 <mohanmk2006mh@gmail.com>
2026-03-04 23:55:21 +0530

    Added heartbeat timestamp to bins

--- models/Bin.js
+++ models/Bin.js
@@ -1,39 +1,46 @@
 const mongoose = require("mongoose");
 
-const binSchema = new mongoose.Schema({
-  binId: {
-    type: String,
-    required: true,
-    unique: true
+const binSchema = new mongoose.Schema(
+  {
+    binId: {
+      type: String,
+      required: true,
+      unique: true
+    },
+
+    distance: {
+      type: Number,
+      required: true
+    },
+
+    fillStatus: {
+      type: String,
+      required: true
+    },
+
+    gasValue: {
+      type: Number,
+      required: true
+    },
+
+    gasStatus: {
+      type: String,
+      required: true
+    },
+
+    rfidAccess: {
+      type: String,
+      default: "None"
+    },
+
+    locked: {
+      type: Boolean,
+      default: false
+    }
   },
-  distance: {
-    type: Number,
-    required: true
-  },
-  fillStatus: {
-    type: String,
-    required: true
-  },
-  gasValue: {
-    type: Number,
-    required: true
-  },
-  gasStatus: {
-    type: String,
-    required: true
-  },
-  rfidAccess: {
-    type: String,
-    default: "None"
-  },
-  locked: {
-    type: Boolean,
-    default: false
-  },
-  timestamp: {
-    type: Date,
-    default: Date.now
+  {
+    timestamps: true
   }
-});
+);
 
 module.exports = mongoose.model("Bin", binSchema);
